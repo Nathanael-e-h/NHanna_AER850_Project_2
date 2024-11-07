9ecb1520a78dc3fbee5b3e09477aa3bf0baae286
Nathanael Hanna <128150666+Nathanael-e-h@users.noreply.github.com>
2024-11-07 16:13:13 -0500

    Nov7_Afternoon
    Getting started. Mostly just image processing so far. Not sure if I actually need the test_generator block b/c the project description says "Create the train and validation generator". I'll figure it out later.

--- Project2CodeSteps1Through4.py
+++ Project2CodeSteps1Through4.py
@@ -1,13 +1,62 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 from tensorflow.keras import datasets, layers, models
-import matplotlib.pyplot as plt
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+
 
 
 ##############################################################################
 # STEP 1. Data Processing
 ##############################################################################
 
-# Define the image sizing
+##### Image sizing
 width, height, channel = 500, 500, 3
-batch_size = 32
+batch_size_setting = 64
+
+##### Modifications
+# Set up data augmentation for training and re-scaling for validation/test
+train_datagen = ImageDataGenerator(
+    rescale=1.0/255,            # Converting pixel RGB ranges to a scale of 0-1. 
+    shear_range=0.15,           # Adding some tilt randomness
+    zoom_range=0.15,            # Adding some zoom randomness
+    horizontal_flip=True        # Adding horizontal flipping randomly
+)
+
+test_datagen = ImageDataGenerator(
+    rescale=1.0/255,            # Converting pixel RGB ranges to a scale of 0-1. 
+    shear_range=0.15,           # Adding some tilt randomness
+    zoom_range=0.15,            # Adding some zoom randomness
+    horizontal_flip=True        # Adding horizontal flipping randomly
+)
+
+
+# No need for the modifications to the last dataset
+validation_datagen = ImageDataGenerator(rescale=1.0/255)
+   
+
+
+
+##### Loading images
+train_generator = train_datagen.flow_from_directory(
+    './Data/train',
+    target_size=(width, height),
+    batch_size=batch_size_setting,
+    class_mode='categorical'  # Use 'categorical' for multi-class classification
+)
+
+validation_generator = validation_datagen.flow_from_directory(
+    './Data/valid',
+    target_size=(width, height),
+    batch_size=batch_size_setting,
+    class_mode='categorical'
+)
+
+test_generator = test_datagen.flow_from_directory(
+    './Data/test',
+    target_size=(width, height),
+    batch_size=batch_size_setting,
+    class_mode='categorical',
+    shuffle=False             # Better for debugging and repeatability
+)
