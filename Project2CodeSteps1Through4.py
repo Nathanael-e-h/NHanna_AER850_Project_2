65fd19767542ef3230ec21ad04edf32c14189111
Nathanael Hanna <128150666+Nathanael-e-h@users.noreply.github.com>
2024-11-14 14:06:33 -0500

    Nov14_afternoon
    Made excellent progress and got good accuracy. Graphing needs tweaking. Not sure if the callback is working as intended? But I got up to 78% so we might be chillin.

--- Project2CodeSteps1Through4.py
+++ Project2CodeSteps1Through4.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 from tensorflow.keras import datasets, layers, models
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
+from tensorflow.keras.callbacks import EarlyStopping
 
 
 
@@ -12,7 +12,7 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 ##############################################################################
 
 ##### Image sizing
-width, height, channel = 500, 500, 3
+width, height, channel = 192, 192, 3
 batch_size_setting = 64
 
 ##### Modifications
@@ -60,3 +60,57 @@ test_generator = test_datagen.flow_from_directory(
     class_mode='categorical',
     shuffle=False             # Better for debugging and repeatability
 )
+
+##############################################################################
+# Step 2: Neural Network Architecture Design 
+##############################################################################
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(width, height, channel)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+
+model.add(layers.Flatten())
+
+model.add(layers.Dense(128, activation='relu'))
+model.add(layers.Dropout(0.5))  # Dropout layer with 50% rate
+model.add(layers.Dense(3, activation='softmax'))  # Softmax for probabilities
+
+
+# debug
+model.summary()
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.CategoricalCrossentropy(),  # Matches one-hot labels
+              metrics=['accuracy'])
+
+early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
+history = model.fit(train_generator, epochs=50,
+                    validation_data=validation_generator,
+                    callbacks=[early_stopping])
+
+
+##### Graphing
+# Plot accuracy 
+plt.figure()
+plt.plot(history.history['accuracy'], label='Training Accuracy')
+plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0, 1])
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+plt.show()
+
+# Is this Loss?
+plt.figure()
+plt.plot(history.history['loss'], label='Training Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.ylim([0, max(max(history.history['loss']), max(history.history['val_loss']))])
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
